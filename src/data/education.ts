ac39cb6487c65088483d3ce83ce8078a59ec16d9
Bhuvan_shetty <bhuvanshetty2018@gmail.com>
2025-02-01 16:51:39 +0530

    commit

--- src/data/education.ts
+++ src/data/education.ts
@@ -3,14 +3,20 @@ import type { Education } from '../types';
 export const educationData: Education[] = [
   {
     degree: 'Master of Computer Applications',
-    institution: 'Vivekananda College of Engineering and Technology',
+    institution: 'Vivekananda College of Engineering and Technology, Puttur',
     period: '2023 - 2025',
     description: 'Specialized in Web Development, DSA & Deep Learning.',
   },
   {
     degree: 'Bachelor of Science in Computer Science',
-    institution: 'St. Aloysius (Autonomous) College',
+    institution: 'St. Aloysius (Autonomous) College, Mangalore',
     period: '2020 - 2023',
     description: 'Graduated with honors. Focus on Software Architecture and Web Technologies.',
   },
+  {
+    degree: 'Pre-University',
+    institution: 'Sri Sathya Sai Loka Seva PU College, Alike',
+    period: '2018 - 2020',
+    description: 'Graduated with PCMB',
+  }
 ];
