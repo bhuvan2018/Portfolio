7b279bf02d815ea82b251662e862570f30d59cfa
Bhuvan_shetty <bhuvanshetty2018@gmail.com>
2025-03-02 00:35:23 +0530

    commit

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -211,7 +211,6 @@ const Projects = () => {
     currentPage * projectsPerPage,
     (currentPage + 1) * projectsPerPage
   );
-
   return (
     <section ref={sectionRef} id="projects" className="py-20 bg-gray-50 dark:bg-gray-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
