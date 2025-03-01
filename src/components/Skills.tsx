8d2879834e27a14ba3d0a781269c19e3b1a19b4a
Bhuvan_shetty <bhuvanshetty2018@gmail.com>
2025-03-02 00:38:09 +0530

    commit

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
