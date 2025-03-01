5a060e4d9cc3222bdeaa5f0ffeed1e038379f221
Bhuvan_shetty <bhuvanshetty2018@gmail.com>
2025-03-02 00:39:39 +0530

    commit

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -40,7 +40,6 @@ const Skills = () => {
     },
     maintainAspectRatio: false,
   };
-
   return (
     <section id="skills" className="py-20 bg-white dark:bg-gray-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
