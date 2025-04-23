84ad8027ffbc88f776c7ff38cc3a818a7cb51879
bhuvan2018 <bhuvanshetty2018@gmail.com>
2025-04-23 20:42:14 +0530

    Remove unused React import from Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import { Github, Linkedin, Twitter, Mail } from 'lucide-react';
 import { WaveEffect } from './animations/WaveEffect';
