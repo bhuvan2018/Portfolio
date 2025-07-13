be67b218b7bf334645c1aa43a189eb383b084238
Bhuvan Shetty <145124594+bhuvan2018@users.noreply.github.com>
2025-07-14 00:01:12 +0530

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -18,27 +18,26 @@ export const projectsData: (Project & { features?: string[] })[] = [
     github: 'https://github.com/bhuvan2018/news_article_classification'
   },
   {
-    id: 3,
+    id: 2,
     title: 'SafeWalk: Ensuring Women Safety in Public Spaces',
     description: 'Mobile App built for women safety',
     longDescription: 'SafeWalk: Ensuring Women Safety in Public Spaces a web app built for the safety of women with features like safe routes, SOS alert messages and many more.',
     image: '/safewalk.png',
-    technologies: ['React Native', 'Next.js', 'TypeScript', 'Framer-Motion','Tailwind-CSS'],
+    technologies: ['React.js', 'Framer-Motion','Tailwind-CSS'],
     features: [
       'SOS Alert',
       'Emergency Live Location Sharing',
       'Safety Routes'
     ],
-    liveDemo: 'http://kzmqu25tzbwjpsduv9lj.lite.vusercontent.net/',
     github: 'https://github.com/bhuvan2018/safe_walk-App',
   },
   {
-    id: 4,
+    id: 3,
     title: 'TechFusion 2K25',
     description: 'A website built for the Department level IT Fest',
     longDescription: 'Welcome to TechFusion 2K25! This website is designed exclusively for 1st & 2nd-year MCA students of VCET to register for various exciting events in our tech fest.',
     image: '/tech-fusion.png',
-    technologies: ['React.js', 'Tailwind-CSS', 'TypeScript','Framer-Motion'],
+    technologies: ['React.js', 'Tailwind-CSS', 'Framer-Motion'],
     features: [
       'Intuitive User Friendly Website',
       'Explore events',
@@ -49,7 +48,7 @@ export const projectsData: (Project & { features?: string[] })[] = [
     github: 'https://github.com/bhuvan2018/TechFusion2K25',
   },
   {
-    id: 5,
+    id: 4,
     title: 'Quiz Master',
     description: 'A user friendly website built for quiz practices.',
     longDescription: 'Welcome to Quiz Master Website! This website is designed exclusively for users who wants to upscale their general knowledge on Tech.',
@@ -65,7 +64,7 @@ export const projectsData: (Project & { features?: string[] })[] = [
     github: 'https://github.com/bhuvan2018/quiztastic-dash',
   },
   {
-    id: 6,
+    id: 5,
     title: 'Pic-to-Toon - Cartoonify Your Memories',
     description: 'A user-friendly app built for transforming your photos into delightful cartoons.',
     longDescription: 'Welcome to Pic-to-Toon! This website lets you transform your photos into delightful cartoons with ease.',
@@ -80,7 +79,7 @@ export const projectsData: (Project & { features?: string[] })[] = [
     github: 'https://github.com/bhuvan2018/Pic-to-Toon',
   },
   {
-    id: 7,
+    id: 6,
     title: 'Real-Time IoT Enabled Smart Fuel Flow Meter',
     description: 'An IoT-enabled Smart Fuel Flow Meter to efficiently monitor and manage fuel usage.',
     longDescription: 'Our IoT-enabled Smart Fuel Flow Meter provides real-time, accurate fuel measurements during refueling, enhancing transaction efficiency and preventing fraud.',
